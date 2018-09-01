a917004a9439557bbe82bd2f23ee9175d1cf5694
lizheming <secretlzm007@gmail.com>
2018-09-01 12:09:08 +0800

    add tina files

---
+++ template/webpack.config.babel.js
@@ -0,0 +1,94 @@
+import webpack from 'webpack'
+import { resolve, dirname, basename } from 'path'
+import MinaEntryPlugin from '@tinajs/mina-entry-webpack-plugin'
+import MinaRuntimePlugin from '@tinajs/mina-runtime-webpack-plugin'
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const loaders = {
+  script: 'babel-loader',
+  style: {
+    loader: 'postcss-loader',
+    options: {
+      config: {
+        path: resolve('./postcss.config.js'),
+      },
+    },
+  },
+}
+
+export default [
+  'plugin/plugin.mina',
+  'miniprogram/app.mina'
+].map(entry => {
+  const context = resolve('src', dirname(entry));
+  const dist = resolve('dist', dirname(entry));
+  entry = basename(entry);
+
+  return {
+    context,
+    entry,
+    output: {
+      path: dist,
+      filename: '[name]',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.mina$/,
+          exclude: /node_modules/,
+          use: [{
+            loader: '@tinajs/mina-loader',
+            options: {
+              loaders,
+            },
+          }],
+        },
+        {
+          test: /\.mina$/,
+          include: /node_modules/,
+          use: '@tinajs/mina-loader',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: loaders.script,
+        },
+        {
+          test: /\.(css|wxss)$/,
+          exclude: /node_modules/,
+          use: loaders.style,
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: 'assets/[name].[hash:6].[ext]',
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      symlinks: true,
+    },
+    plugins: [
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: 'development',
+        DEBUG: false,
+      }),
+      new MinaEntryPlugin(),
+      new MinaRuntimePlugin({
+        runtime: './common.js',
+      }),
+      // new webpack.optimize.CommonsChunkPlugin({
+      //   name: 'common.js',
+      //   minChunks: 2,
+      // }),
+      isProduction && new UglifyJsPlugin(),
+    ].filter(Boolean),
+  };
+});
