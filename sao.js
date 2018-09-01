0ea2276f07688d300465e35984c701ae5acbed4a
lizheming <secretlzm007@gmail.com>
2018-09-01 16:37:56 +0800

    update sao config

--- sao.js
+++ sao.js
@@ -3,26 +3,26 @@ const superb = require('superb')
 module.exports = {
   prompts: {
     name: {
-      message: 'What is the name of the new project?',
+      message: 'Plugin name?',
       default: ':folderName:'
     },
     description: {
-      message: 'How would you descripe the new project?',
+      message: 'Description:',
       default: `my ${superb()} project`
     },
     username: {
-      message: 'What is your GitHub username?',
+      message: 'Your name:',
       default: ':gitUser:',
       store: true
     },
     email: {
-      message: 'What is your GitHub email?',
+      message: 'Your email:',
       default: ':gitEmail:',
       store: true
     },
     website: {
       message: 'The URL of your website?',
-      default({username}) {
+      default({ username }) {
         return `github.com/${username}`
       },
       store: true
@@ -33,5 +33,16 @@ module.exports = {
   },
   showTip: true,
   gitInit: true,
-  installDependencies: true
+  installDependencies: true,
+  post({ log, isNewFolder, folderName, chalk }) {
+    console.log()
+    log.success(`Successfully generated into ${chalk.cyan(folderName)}`)
+    console.log()
+    console.log(chalk.bold('To get started:\n'))
+    if (isNewFolder) {
+      console.log(` cd ${folderName}`)
+    }
+    console.log(' npm start')
+    console.log()
+  }
 }
